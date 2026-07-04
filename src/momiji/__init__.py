ad81df49660be6c378c71424add7b12cb9a4da41
nercone-dev <nercone@nercone.dev>
2026-07-04 22:58:03 +0900

    --

--- src/momiji/__init__.py
+++ src/momiji/__init__.py
@@ -1,4 +1,4 @@
-from .server import Handler, Server
+from .server import Handler, Server, Listener, IPVersion
 from .models import Role, Message, Request, Response
 from .headers import Headers, CommaHeader, Link, AcceptEncoding, ContentType, ETag
 from .protocol import Connection, Protocol
@@ -6,4 +6,4 @@ from .finalizer import finalize_request, finalize_response
 from .responses import PlainTextResponse, HTMLResponse, JSONResponse, FileResponse, RedirectResponse
 from .errors import HTTPError, HTTPViolationError, HTTPReportedViolationError, WebSocketProtocolError
 
-__all__ = ["Handler", "Server", "Role", "Message", "Request", "Response", "Headers", "CommaHeader", "Link", "AcceptEncoding", "ContentType", "ETag", "Connection", "Protocol", "finalize_request", "finalize_response", "PlainTextResponse", "HTMLResponse", "JSONResponse", "FileResponse", "RedirectResponse", "HTTPViolationError", "HTTPError", "HTTPReportedViolationError", "WebSocketProtocolError"]
+__all__ = ["IPVersion", "Listener", "Handler", "Server", "Role", "Message", "Request", "Response", "Headers", "CommaHeader", "Link", "AcceptEncoding", "ContentType", "ETag", "Connection", "Protocol", "finalize_request", "finalize_response", "PlainTextResponse", "HTMLResponse", "JSONResponse", "FileResponse", "RedirectResponse", "HTTPViolationError", "HTTPError", "HTTPReportedViolationError", "WebSocketProtocolError"]
--- src/momiji/server.py
+++ src/momiji/server.py
@@ -1,10 +1,63 @@
+import os
+import signal
 import socket
 import uvloop
 import asyncio
+from enum import Enum
 from typing import Optional, Callable
+from dataclasses import dataclass
 
 from .models import Role
 from .protocol import Protocol
+
+class IPVersion(Enum):
+    IPv4 = socket.AF_INET
+    IPv6 = socket.AF_INET6
+
+@dataclass(frozen=True)
+class Listener:
+    ip_version: Optional[IPVersion] = None
+    port: Optional[int] = None
+    path: Optional[str] = None
+
+    def __post_init__(self):
+        if (self.port is None) == (self.path is None):
+            raise ValueError("specify exactly one of port or path")
+
+        if self.port is not None and self.ip_version is None:
+            raise ValueError("ip_version is required when port is specified")
+
+        if self.path is not None and self.ip_version is not None:
+            raise ValueError("ip_version must not be specified when path is specified")
+
+    def bind(self, reuse_port: bool = False) -> socket.socket:
+        if self.is_uds:
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+            try:
+                os.unlink(self.path)
+            except FileNotFoundError:
+                pass
+
+            sock.bind(self.path)
+        else:
+            sock = socket.socket(self.ip_version.value, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            if reuse_port:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+            address = "" if self.ip_version is IPVersion.IPv4 else "::"
+            sock.bind((address, self.port))
+
+        sock.listen()
+        sock.setblocking(False)
+
+        return sock
+
+    @property
+    def is_uds(self) -> bool:
+        return self.path is not None
 
 class Handler:
     def __init__(self, on_request: Optional[Callable] = None, on_websocket: Optional[Callable] = None):
@@ -17,7 +70,54 @@ class Server:
         self.handler = handler or Handler()
         self.upstream = upstream
 
-    def run(self, sockets: list[socket.socket]):
+    def run(self, listeners: list[Listener], workers: int = 0):
+        if workers < 0:
+            raise ValueError("workers must be at least 0")
+
+        if workers == 0:
+            sockets = [listener.bind() for listener in listeners]
+            self.run_worker(sockets)
+            return
+
+        shared_sockets = [listener.bind() for listener in listeners if listener.is_unix]
+        tcp_listeners = [listener for listener in listeners if not listener.is_unix]
+
+        pids = [self.fork_worker(shared_sockets, tcp_listeners) for _ in range(workers)]
+
+        def forward_signal(signum, frame):
+            for pid in pids:
+                try:
+                    os.kill(pid, signum)
+                except ProcessLookupError:
+                    pass
+
+        signal.signal(signal.SIGTERM, forward_signal)
+        signal.signal(signal.SIGINT, forward_signal)
+
+        while pids:
+            try:
+                pid, _ = os.wait()
+            except ChildProcessError:
+                break
+            except InterruptedError:
+                continue
+            else:
+                pids.remove(pid)
+
+    def fork_worker(self, shared_sockets: list[socket.socket], tcp_listeners: list[Listener]) -> int:
+        pid = os.fork()
+
+        if pid == 0:
+            signal.signal(signal.SIGTERM, signal.SIG_DFL)
+            signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+            own_sockets = [listener.bind(reuse_port=True) for listener in tcp_listeners]
+            self.run_worker(shared_sockets + own_sockets)
+            os._exit(0)
+
+        return pid
+
+    def run_worker(self, sockets: list[socket.socket]):
         uvloop.install()
         asyncio.run(self.serve(sockets))
 
